2153967065e05827d486d582b4f2f31cb851fbcb
Jay McCarthy <jay.mccarthy@gmail.com>
2012-08-21 19:35:46 -0600

    contracts

--- model.rkt
+++ model.rkt
@@ -1,16 +1,42 @@
 #lang racket/base
 (require racket/date
+         racket/contract
          racket/serialize)
 
 (struct assignment (normal-weight optional-weight id due-secs eval-secs peer-secs questions) #:prefab)
 (struct question (normal-weight optional-weight prompt type) #:prefab)
 
 (struct question-self-eval
-  (self-score file line-number) #:prefab);TODO merge file and line-number
+        (self-score file line-number) #:prefab);TODO merge file and line-number
 
 (struct peer-grading-struct
-  (peer-id question-grades) #:prefab)
+        (peer-id question-grades) #:prefab)
 
 (struct student (nickname firstname lastname email) #:prefab)
 
-(provide (all-defined-out))
+(provide/contract
+ [struct assignment
+         ([normal-weight number?]
+          [optional-weight number?]
+          [id string?]
+          [due-secs number?]
+          [eval-secs number?]
+          [peer-secs (or/c false/c number?)]
+          [questions (listof question?)])]
+ [struct question
+         ([normal-weight number?]
+          [optional-weight number?]
+          [prompt string?]
+          [type (or/c 'bool 'numeric)])]
+ [struct question-self-eval
+         ([self-score number?]
+          [file string?]
+          [line-number string?])]
+ [struct peer-grading-struct
+         ([peer-id string?]
+          [question-grades (listof question-self-eval?)])]
+ [struct student
+         ([nickname string?]
+          [firstname string?]
+          [lastname string?]
+          [email string?])])
